b16017077dc00cfe7acc624f8f2ccca20eb6a439
viniciuzvieira <viniciusvieira04@hotmail.com>
2019-05-23 15:47:06 -0300

    teste 404

--- wp-content/themes/realestate-7/404.php
+++ wp-content/themes/realestate-7/404.php
@@ -13,6 +13,7 @@ echo '<div class="container">'; ?>
         <article class="col span_12">
         
             <h1><?php esc_html_e('Page Not Found', 'contempo'); ?></h1>
+            <h2>teste</h2>
             <p class="lead"><?php esc_html_e('Woah there! Looks like you\'ve gotten lost.', 'contempo'); ?></p>
             <a class="btn" href="<?php echo home_url(); ?>"><?php esc_html_e('Go back to the homepage', 'contempo'); ?></a>
 
